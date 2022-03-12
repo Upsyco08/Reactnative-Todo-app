2e9d13cac2afcc5d962fa58c3c5f47fda5627af5
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-03-12 10:38:20 +0545

    Added all files-first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Todo from "./components/Todo";
+
+export default function App() {
+  return <Todo />;
+}
