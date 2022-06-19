777a0fd2745b3498e5a0bc03eeaf94dd7fbe34c0
Sandesh Raj Shakya <53856036+Upsyco08@users.noreply.github.com>
2022-06-19 22:46:14 +0545

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Todo from "./components/Todo";
-
-export default function App() {
-  return <Todo />;
-}
